9789ddb24699e929b9e496e6a70ccdf135dbca2a
uni-fied <devadiroot@gmail.com>
2022-07-01 13:39:13 +0700

    Update controller v1.0 Beta 🦅

--- api/manajemen-keuangan/config/controller.php
+++ api/manajemen-keuangan/config/controller.php
@@ -1,24 +1,40 @@
 <?php
 
-    $switch_server = "LOCAL";
+    $switch_server = "HOSTING";
 
+    # Deploy to New Hosting @ 01 Juni 2022 09.11 WIB - V1.0 BETA
+
+    # Version API
     $versionapp_api                       = "V1.0 BETA";
     $build_version_api                    = "Build 07062022.0935.1";
+
+    # 1. Build 30042022.110910
+    # 2. Build 07062022.0935.1
  
     switch ($switch_server) {
         case "LOCAL":
-            $server_db                 = "Lokal Database Komputer";
-            $host_db                   = "host_db";
-            $username_db               = "username_db";
-            $key_db                    = "password_db";
-            $db_name                   = "database_name";
+            # Server Local Komputer
+            $server_db                 = "Lokal Komputer";
+            $host_db                   = "127.0.0.1";
+            $username_db               = "root";
+            $key_db                    = "";
+            $db_name                   = "lts_db_managerial_dev";
             break;
         case "HOSTING":
-            $server_db                 = "Server Hosting Database";
-            $host_db                   = "host_db";
-            $username_db               = "username_db";
-            $key_db                    = "password_db";
-            $db_name                   = "database_name";
+            # Server 000webhost
+            // $server_db                 = "Server Hosting";
+            // $host_db                   = "localhost";
+            // $username_db               = "id18790144_academydev";
+            // $key_db                    = "453p714D1!@1234";
+            // $db_name                   = "id18790144_lts_db_academydev";
+            
+            # Server InfinityFree
+            # New User @ 01 Juli 2022
+            $server_db                 = "Server Hosting";
+            $host_db                   = "fdb32.awardspace.net";
+            $username_db               = "4130798_softask";
+            $key_db                    = "453p714d1";
+            $db_name                   = "4130798_softask";
             break;  
     }
 
